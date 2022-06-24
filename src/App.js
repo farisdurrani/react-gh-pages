099fa4943bc3d523eb6f42ef3c976015404b163f
Faris Durrani <afarisdurrani@gmail.com>
2022-06-24 19:19:59 -0400

    up

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save frdsxto reload.
+          Edit <code>src/App.js</code> and save 3435frdsxto reload.
         </p>
         <a
           className="App-link"
